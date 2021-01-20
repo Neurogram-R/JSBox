18c330170941be7838decec2a78fe57f23a0f8ba
Neurogram <greedyage@hotmail.com>
2021-01-20 16:09:43 +0800

    add language, random post

--- Widgets/Instagets.js
+++ Widgets/Instagets.js
@@ -2,15 +2,26 @@
 
 Instagets by Neurogram
 
- - Fill Instagram web login cookie in line 13 of the script
+ - Fill Instagram web login cookie in line 16 of the script
  - Fill Instagram username in Input Value of widget.
  - Tap image to open post
  - Tap profile to open user profile (medium widget only)
+ - Set language in line 17 of the script
+ - Set random post in line 18 of the script
+ - Set random children post in line 19 of the script
 
 */
 
 const inputValue = $widget.inputValue;
 const cookie = `LOGIN_COOKIE`
+const language = "en" // en or cn
+const random_post_max = 1 // max range, 1 for lastest post
+const random_children_post = true // true or false
+
+const edge_type_label = {
+    "en": ["posts", "followers", "following"],
+    "cn": ["帖子", "粉丝", "关注"]
+}
 
 if (inputValue) {
     let instagram_url = "https://www.instagram.com/" + inputValue
@@ -24,13 +35,13 @@ if (inputValue) {
     share_data = share_data[0].replace(/window._sharedData = |;<\/script>/g, "")
     share_data = JSON.parse(share_data).entry_data.ProfilePage[0].graphql.user
 
-    let counters = resp.data.match(/og:description" content=".+/)
-    counters = counters[0].replace(/og:description" content="|\s-\s.+/g, "").replace(/, /g, " ").split(/ /)
+    let counters = [numFormatter(share_data.edge_owner_to_timeline_media.count, 1), numFormatter(share_data.edge_followed_by.count, 1), numFormatter(share_data.edge_follow.count, 1)]
+    let type = edge_type_label[language]
 
     let counter_view = []
     let type_view = []
 
-    for (var i = 0; i < counters.length; i = i + 2) {
+    for (var i = 0; i < counters.length; i++) {
         counter_view.push({
             type: "text",
             props: {
@@ -45,7 +56,7 @@ if (inputValue) {
         type_view.push({
             type: "text",
             props: {
-                text: counters[i + 1],
+                text: type[i],
                 font: $font(10),
                 color: $color("#aaaaaa"),
                 minimumScaleFactor: 0.5,
@@ -60,7 +71,19 @@ if (inputValue) {
             const family = ctx.family;
             const width = $widget.displaySize.width
             const height = $widget.displaySize.height
-            const lastest_share = share_data.edge_owner_to_timeline_media.edges[0].node
+            let share_total = share_data.edge_owner_to_timeline_media.edges
+
+            if (random_post_max > share_total.length) {
+                random_post_max = share_total.length
+            }
+
+            let current_share = share_total[Random(0, random_post_max - 1)].node
+            let display_url = current_share.display_url
+
+            if (random_children_post && current_share.edge_sidecar_to_children) {
+                let children = current_share.edge_sidecar_to_children.edges
+                display_url = children[Random(0, children.length - 1)].node.display_url
+            }
 
             let small_widget = {
                 type: "zstack",
@@ -76,10 +99,10 @@ if (inputValue) {
                     {
                         type: "image",
                         props: {
-                            uri: lastest_share.display_url,
+                            uri: display_url,
                             resizable: true,
                             scaledToFill: true,
-                            widgetURL: "https://www.instagram.com/p/" + lastest_share.shortcode
+                            widgetURL: "https://www.instagram.com/p/" + current_share.shortcode
                         }
                     },
                     {
@@ -93,7 +116,7 @@ if (inputValue) {
                             {
                                 type: "text",
                                 props: {
-                                    text: `${family == 2 ? "     " : "    "}♥️ ${lastest_share.edge_liked_by.count} 💬 ${lastest_share.edge_media_to_comment.count}`,
+                                    text: `${family == 2 ? "     " : "    "}♥️ ${numFormatter(current_share.edge_liked_by.count, 1)} 💬 ${numFormatter(current_share.edge_media_to_comment.count, 1)}`,
                                     font: family == 2 ? $font(13) : $font(9),
                                     color: $color("white"),
                                     minimumScaleFactor: 0.5,
@@ -220,4 +243,28 @@ function spacerMaker(height, width) {
             }
         }
     }
+}
+
+function numFormatter(num, digits) {
+    const si = [
+        { value: 1, symbol: "" },
+        { value: 1E3, symbol: "K" },
+        { value: 1E6, symbol: "M" },
+        { value: 1E9, symbol: "G" },
+        { value: 1E12, symbol: "T" },
+        { value: 1E15, symbol: "P" },
+        { value: 1E18, symbol: "E" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    let i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].value) {
+            break;
+        }
+    }
+    return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
+
+function Random(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
 }
